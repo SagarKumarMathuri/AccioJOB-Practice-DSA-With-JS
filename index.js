1101a89659c5a76af075f342103a6d9eddf69064
sagar kumar mathuri <sagarku2002@gmail.com>
2026-05-29 13:17:21 +0530

    DSA

---
+++ index.js
@@ -0,0 +1,17 @@
+// Printing Messages in Different Languages
+
+// For JavaScript:
+console.log("Hello, World.");
+console.log("Hello, JavaScript.")
+
+// For Java:
+System.out.println("Hello, World.");
+System.out.println("Hello, JavaScript.");
+
+
+// For C++:
+
+cout<<"Hello, World.\n";
+cout<<"Hello, JavaScript.";
+
+
