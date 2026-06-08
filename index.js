4368506625a8fd2f0c0f380a15084fea61b6ca61
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-08 17:38:28 +0530

    Staircase

--- index.js
+++ index.js
@@ -336,3 +336,11 @@ if((year % 4===0) && (year % 100 != 0) || (year % 400 === 0)){
 
 //Sum of Even Numbers
 
+let n = 12;
+let sum = 0 ;
+for(let i = 1; i <= n; i++){
+  if(i%2==0){
+    sum += i;
+  }
+}
+console.log(sum);
