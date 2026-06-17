a789198100f403dae69c6c6d36a8ce00272d48bc
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-17 11:10:17 +0530

    5. Left Rotate Array by One

--- index.js
+++ index.js
@@ -507,7 +507,20 @@ if((year % 4===0) && (year % 100 != 0) || (year % 400 === 0)){
 
 // Move Zeros to End
 
+// function moveZeros(arr) {
+//     let count = 0; 
 
+//     for(let i= 0; i < arr.length; i++){
+//         if (arr[i] !== 0) {
+//             [arr[count], arr[i]] = [arr[i], arr[count]];
+//             count++;
+//         }
+//     }
+//     return arr;
+// }
+// console.log(moveZeros([2,3,0,50,0,4,0,2]));
+
+// 
 
 
 
