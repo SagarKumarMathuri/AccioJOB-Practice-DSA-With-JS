06a56bbc5e82984bb02cb366909a0b39602d1e7b
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-17 11:04:32 +0530

    Move Zeros to End

--- index.js
+++ index.js
@@ -491,6 +491,21 @@ if((year % 4===0) && (year % 100 != 0) || (year % 400 === 0)){
 
 // 3. Reverse Array
 
+// function reverseArray(arr) {
+//     let left = 0;
+//     let right = arr.length-1;
+
+//     while (left < right) {
+//         [arr[left], arr[right]] = [arr[right], arr[left]];
+//         left++;
+//         right--;
+//     }
+//     return arr
+// }
+
+// console.log(reverseArray([1,2,3,4,6]));
+
+// Move Zeros to End
 
 
 
