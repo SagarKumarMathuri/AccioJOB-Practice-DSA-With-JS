0adfa46be9aee92b8e23790b385fdf6d91c37f27
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-21 23:59:39 +0530

    saa

--- index.js
+++ index.js
@@ -737,6 +737,22 @@ if((year % 4===0) && (year % 100 != 0) || (year % 400 === 0)){
 
 // 3. Largest Element in Matrix
 
+// function lagestElement(mat) {
+//     let max = mat[0][0];
+
+//     for(let i = 0; i < mat.length; i++){
+//         for(let j = 0; j < mat[i].length; j++){
+//             max = Math.max(max, mat[i][j]);
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(lagestElement([
+//     [1,2,5],
+//     [4,6,8]
+// ]));
+
 
 
 
