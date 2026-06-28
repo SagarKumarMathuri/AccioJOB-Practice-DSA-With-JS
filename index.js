7a2dd768d5a068aa144dd0f7b07ae0132feb4975
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-28 22:02:01 +0530

    4. Print Inverted Pyramid Pattern

--- index.js
+++ index.js
@@ -850,6 +850,23 @@ if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
 
 // 3. Print Pyramid Pattern
 
+// let row = 5;
+
+// for(let i = 1; i <= 2 * row -1; i+=2){
+//   let str = ''
+//   const currentRow = (i + 1) / 2
+
+//   for(let j = 1; j <= row + currentRow -1; j++){
+//     if(j <= row - currentRow) {
+//       str += ' '
+//     } else {
+//       str += '*'
+//     }
+//   }
+//   console.log(str);
+  
+// }
+
 
 
 
