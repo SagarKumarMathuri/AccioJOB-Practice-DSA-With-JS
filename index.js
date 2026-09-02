4bd28cdeb407875dd23814c602e79de10413ed38
sagar kumar mathuri <sagarku2002@gmail.com>
2026-09-02 10:47:07 +0530

    Iterative Insertion Sort

--- index.js
+++ index.js
@@ -2394,3 +2394,21 @@
 // }
 // let arr = [5,2,8,1,9,3];
 // console.log(bubbleSortDescending(arr));
+
+// Iterative Insertion Sort
+function insertionSort(arr){
+  for(let i = 1; i < arr.length; i++){
+    let key = arr[i];
+    let j = i - 1;
+
+    while(j >= 0 && arr[j] > key){
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr
+}
+
+let arr = [5,2,8,1,9,3]
+console.log(insertionSort(arr));
